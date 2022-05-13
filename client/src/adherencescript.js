35649762c56030a95c93276a26bd12c39455c12b
DrMoE77 <drmo.afifi@gmail.com>
2022-05-13 23:41:05 +1000

    adherencecalc

--- client/src/adherencescript.js
+++ client/src/adherencescript.js
@@ -1,3 +1,11 @@
 function percentage(partialValue, totalValue) {
   return (100 * partialValue) / totalValue;
 } 
+
+var adh = Number
+var dosesTaken = Number
+var dosesPrescribed = Number
+
+function adherance(){
+  adh
+}
