922612d0e9ed6be12af2629442174d84c02b4161
DrMoE77 <drmo.afifi@gmail.com>
2022-06-03 21:59:49 +1000

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://DrMoE77:Yasseen20157@cluster0.qusi2.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://"DrMoE77:Yasseen20157"@cluster0.qusi2.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
