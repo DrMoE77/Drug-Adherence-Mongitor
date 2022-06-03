fa9052b37e1124edd6d28e66b7cf98852dc11447
DrMoE77 <drmo.afifi@gmail.com>
2022-06-03 22:06:05 +1000

    mongoDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || http://127.0.0.1:27017/welcomeU-db, {
+mongoose.connect(process.env.MONGODB_URI || http://127.0.0.1:27017/welcomeU-db,
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
